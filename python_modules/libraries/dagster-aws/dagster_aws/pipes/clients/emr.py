f6fcde399842febb822843c542faa451015c5215
Maxime Armstrong <maxime@retain.ai>
2025-01-07 10:10:08 -0500

    [integrations-api][ga] PipesEMRClient in dagster-aws

--- python_modules/libraries/dagster-aws/dagster_aws/pipes/clients/emr.py
+++ python_modules/libraries/dagster-aws/dagster_aws/pipes/clients/emr.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, cast
 import boto3
 import dagster._check as check
 from dagster import MetadataValue, PipesClient
-from dagster._annotations import experimental, public
+from dagster._annotations import public
 from dagster._core.definitions.metadata import RawMetadataMapping
 from dagster._core.definitions.resource_annotation import TreatAsResourceParam
 from dagster._core.errors import DagsterExecutionInterruptedError
@@ -70,7 +70,6 @@ def add_configuration(
 
 
 @public
-@experimental
 class PipesEMRClient(PipesClient, TreatAsResourceParam):
     """A pipes client for running jobs on AWS EMR.
 
